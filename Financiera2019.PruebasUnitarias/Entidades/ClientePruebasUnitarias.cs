efe8dd9def15feebe65117bb5e4626ff36021211
p-cuadros <patcuadrosq@upt.pe>
2019-04-01 20:14:49 -0500

    pruebas unitarias y de datos

---
+++ Financiera2019.PruebasUnitarias/Entidades/ClientePruebasUnitarias.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Financiera2019.Dominio.Entidades;
+using Financiera2019.Dominio.Repositorios;
+using Financiera2019.Infraestructura.Datos.EF.Repositorios;
+
+namespace Financiera2019.PruebasUnitarias.Entidades
+{
+    [TestClass]
+    public class ClientePruebasUnitarias
+    {
+        [TestMethod]
+        public void CrearClienteSatisfactoriamente()
+        {
+            var cliente = Cliente.Crear(1, "Juan Perez");
+            Assert.IsNotNull(cliente);
+            Assert.AreEqual(cliente.CodigoCliente, 1);
+        }
+        [TestMethod]
+        public void RegistrarClienteEnBDSatisfactoriamente()
+        {
+            var cliente = Cliente.Crear(1, "Juan Perez");
+            IRepositorio rep = new Repositorio();
+            rep.Adicionar(cliente);
+            rep.GuardarCambios();
+
+            Assert.IsNotNull(cliente);
+            Assert.AreEqual(cliente.CodigoCliente, 1);
+        }
+        [TestMethod]
+        public void BuscarClienteEnBDSatisfactoriamente()
+        {
+            IRepositorio rep = new Repositorio();
+            var cliente = rep.ObtenerPorCodigo<Cliente>(1);
+
+            Assert.IsNotNull(cliente);
+            Assert.AreEqual(cliente.CodigoCliente, 1);
+        }
+    }
+}
